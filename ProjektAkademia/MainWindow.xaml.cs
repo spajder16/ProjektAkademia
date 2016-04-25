309bb68098f5c3818797a64af75c5bdffd2c43f9
Kamil <Kamil@KAMILLAP>
2016-04-25 20:11:51 +0200

    Road

--- ProjektAkademia/MainWindow.xaml.cs
+++ ProjektAkademia/MainWindow.xaml.cs
@@ -22,13 +22,18 @@ namespace ProjektAkademia
     public partial class MainWindow : Window
     {
         private DispatcherTimer timer = new DispatcherTimer(); // timer object
+        private DispatcherTimer timer2 = new DispatcherTimer();
         List<Figure> myElements = new List<Figure>();
+        List<Point> myRoad = new List<Point>();
         public MainWindow()
         {
             InitializeComponent();
             timer.Interval = TimeSpan.FromMilliseconds(50); // update 20 times/second
             timer.Tick += TimerTick;
             timer.Start();
+            timer2.Interval = TimeSpan.FromMilliseconds(10); // update 20 times/second
+            timer2.Tick += TimerTick2;
+            
 
         }
         private void TimerTick(object sender, EventArgs e)
@@ -44,6 +49,17 @@ namespace ProjektAkademia
             
             //Canvas.SetLeft()
         }
+
+        private void TimerTick2(object sender, EventArgs e)
+        {
+            // movement in one interval
+
+            // update position
+            myRoad.Add(new Point(Mouse.GetPosition(this.Pole).X, Mouse.GetPosition(this.Pole).Y));
+
+            //Canvas.SetLeft()
+        }
+
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
             Random rand = new Random(DateTime.Now.Ticks.GetHashCode());
@@ -69,7 +85,9 @@ namespace ProjektAkademia
         {
             if(e.LeftButton == MouseButtonState.Pressed)
             {
-               
+                timer2.Start();
+                myRoad.Clear();
+                
                 Random rand = new Random();
                 Point position = new Point(Mouse.GetPosition(this.Pole).X, Mouse.GetPosition(this.Pole).Y);
                 myElements.Add(new Prostokat(rand, position));
@@ -87,6 +105,34 @@ namespace ProjektAkademia
                     }
                 }
             }
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                if (myElements.Count != 0)
+                {
+                    Random rand = new Random(2);
+                    foreach (var element in myElements)
+                    {
+                        element.Speed = new Point(rand.Next(1000), rand.Next(1000));
+                    }
+                }
+            }
+        }
+
+        private void Pole_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            timer2.Stop();
+            for (int i = 0; i < myRoad.Count-1; i++)
+            {
+                Line myLine = new Line();
+                myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+                myLine.X1 = myRoad.ElementAt<Point>(i).x;
+                myLine.X2 = myRoad.ElementAt<Point>(i+1).x;
+                myLine.Y1 = myRoad.ElementAt<Point>(i).y;
+                myLine.Y2 = myRoad.ElementAt<Point>(i+1).y;
+                Pole.Children.Add(myLine);
+            }
+             
+            Pole.UpdateLayout();
         }
     }
 }
